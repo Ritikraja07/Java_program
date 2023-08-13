8e871f01485aa4cd3010a4951189d1d1e9ea6f27
Ritikraja07 <Ritik.rock121@gmail.com>
2023-08-13 12:16:47 +0530

    Added a file

---
+++ input_operator_calculator.java
@@ -0,0 +1,50 @@
+// Create a calculator using switch statement to perform addition, subtraction, multiplication and division.
+
+import java.util.*;
+
+public class switchassignment {
+    public static void main(String[] args) {
+        char op;
+        double num1, num2, ans;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an operator('+' '-' '*' '/'): ");
+        op=sc.next().charAt(0);
+        
+        System.out.print("Enter first number: ");
+        num1=sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        num2=sc.nextInt();
+
+        switch (op) {
+            case '+':
+            // Addition
+                ans=num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + ans);
+                break;
+            
+            case '-':
+            // Subtraction
+                ans=num1 - num2;
+                System.out.println(num1 + "-" + num2 + " = " + ans);
+                break;
+
+            case '*':
+            // Multiplicataion
+                ans=num1 * num2;
+                System.out.println(num1 + "*" + num2 + " = " + ans);
+                break;
+
+            case '/':
+            // Division
+                ans=num1 / num2;
+                System.out.println(num1 + "/" + num2 + " = " + ans);
+                break;
+
+            default:
+                System.out.println("Wrong input");
+        }
+        sc.close();
+    }
+}
