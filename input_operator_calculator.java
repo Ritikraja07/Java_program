dcb2bbcf53c957ac8f8ce24732cd3683175f9584
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2023-08-23 19:13:33 +0530

    Update input_operator_calculator.java

--- input_operator_calculator.java
+++ input_operator_calculator.java
@@ -32,6 +32,7 @@ public class switchassignment {
             case '-':
             // Subtraction
                 ans=num1 - num2;
+                // Print the answer
                 System.out.println(num1 + "-" + num2 + " = " + ans);
                 break;
 
