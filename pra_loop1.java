d1406a91fe256cb2a65f3a1d6fc95fec56cde02a
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2023-08-30 10:40:10 +0530

    Update pra_loop1.java

--- pra_loop1.java
+++ pra_loop1.java
@@ -7,6 +7,7 @@ public class pra_loop1 {
         // Reverse the digits of a number.
         // using scanner class
         Scanner sc = new Scanner(System.in);
+        // Taking an integer value
         int n = sc.nextInt();
 
         int ans = 0;
